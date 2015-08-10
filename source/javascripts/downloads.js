1f088b488240660550630a6e97e4da04ce5949ef
Robert Faulk <robertf@spiceworks.com>
2015-08-10 17:35:55 -0500

    Created initial version of OS tabs

--- source/javascripts/downloads.js
+++ source/javascripts/downloads.js
@@ -23,4 +23,31 @@
     }
   };
 
+  $(document).ready(function(){
+    $("ul.download-box_tab-header li").click(function(e){
+      console.log('click!');
+      if (!$(this).hasClass("active") && !$(this).hasClass("filler")) {
+        var tabNum = $(this).index();
+        var nthChild = tabNum+1;
+        $("ul.download-box_tab-header li.active").removeClass("active");
+        $(this).addClass("active");
+        $("ul.download-box_tab-body li.active").removeClass("active");
+        $("ul.download-box_tab-body li:nth-child("+nthChild+")").addClass("active");
+      }
+    });
+  });
+
+  $(document).ready(function(){
+    $(".download-box_button").click(function(e){
+      console.log('CLEEK');
+      console.log($(".download-box_splash"));
+      $(".download-box_splash").addClass("download-box_hidden");
+      console.log('A');
+      $("ul.download-box_tab-header").removeClass("download-box_hidden");
+      console.log('B');
+      $("ul.download-box_tab-body").removeClass("download-box_hidden");
+      console.log('C');
+    });
+  });
+
 }(jQuery));
